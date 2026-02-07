9e4fcf956fdbe0ccd885232b9bd1b2c0edf5bd48
Ninad09 <ninadgaonkar234@gmail.com>
2026-02-07 20:41:33 +0530

    feat: custom hook to fetch parent messages

---
+++ hooks/useMessage.ts
@@ -0,0 +1,24 @@
+// hooks/useInbox.ts
+import { Parent, Message } from "@/db/generated/prisma"
+import { useState, useEffect } from "react"
+
+type MessageWithParent = Message & { parent: Parent | null }
+
+export function useInbox(teacherId?: string) {
+  const [messages, setMessages] = useState<MessageWithParent[]>([])
+
+  useEffect(() => {
+    if (!teacherId) return
+
+    const fetchInbox = async () => {
+      const res = await fetch(`/api/messages/inbox?teacherId=${teacherId}`)
+      if (res.ok) setMessages(await res.json())
+    }
+
+    fetchInbox()
+    // const interval = setInterval(fetchInbox, 3000)
+    // return () => clearInterval(interval)
+  }, [teacherId])
+
+  return { messages }
+}
